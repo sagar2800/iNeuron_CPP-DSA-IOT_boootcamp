cea4284c0a238892ac474297dc65c469b9bad487
sagar2800 <68487060+sagar2800@users.noreply.github.com>
2023-04-09 20:31:15 +0530

    Update Assignment_08.c

--- C/Assignment_08.c
+++ C/Assignment_08.c
@@ -8,6 +8,18 @@
 * * * * *
 
 */
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question2.
 
@@ -16,8 +28,25 @@
     * * *
   * * * *
 * * * * *
-
 */
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i = 5; i > 0; i--)
+    {
+        for(j = 1; j <= 5; j++)
+        {
+            if((i-j) > 0)
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
 
 /*Question3.
 
@@ -28,6 +57,24 @@
 *
 
 */
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 5; j > 0; j--)
+        {
+            if(((j+1) - i) > 0)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
 
 /*Question4.
 
@@ -38,6 +85,24 @@
         * 
         
 */
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1 ; j <= 5; j++)
+        {
+            if(i <= j)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
 
 /*Question5.
 
@@ -48,7 +113,29 @@
 * * * * * * * * *
 
 */
-
+#include<stdio.h>
+int main()
+{
+    int columns,rows = 9,i,j,count; //columns are vertical // rows are horizontal
+    for(columns = 1; columns <= 5; columns++)
+    {
+        j = 0;
+        for(i = 1; i <= 9; i++)
+        {
+            if(i <= (9 - 4 - columns))
+                printf(" ");
+            else
+                {
+                    j++;
+                    if(j <= columns * 2 - 1)
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+        }
+        printf("\n");
+    }
+}
 /*Question6.
 
 * * * * * * * * *
@@ -58,6 +145,30 @@
         *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows = 9,i,j; //columns are vertical // rows are horizontal
+    for(columns = 1; columns <= 5; columns++)
+    {
+        j = 0;
+        for(i = 1; i <= 9; i++)
+        {
+            if(i <= columns - 1)
+                printf(" ");
+            else
+            {
+                j++;
+                if(j <= rows + 1 - (columns * 2 - 1))
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 
 /*Question7.
@@ -69,6 +180,41 @@
 *                 *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,j,k,count = 0;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        j = 1;
+        k = 1;
+        count = 0;
+        for(rows = 1; rows <= 10; rows++)
+        {
+            if(j <= 5 - (columns - 1))
+            {
+                printf("*");
+                count++;
+                j++;
+            }
+            else
+            {
+                if(k <= (5 - count) * 2  && columns != 1)
+                {
+                    printf(" ");
+                    k++;
+                }
+                else
+                {
+                    j = 1;
+                    rows--;
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 
 /*Question8.
@@ -78,6 +224,49 @@
 1 2 3 4 3 2 1
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,j,k,count;
+    for(columns = 1; columns <= 4; columns++)
+    {
+        j = columns;
+        k = 1;
+        count = 0;
+        for(rows = 1; rows <= 7; rows++)
+        {
+            if(4 - j > 0)
+            {
+                printf(" ");
+                j++;
+                count++;
+            }
+            else
+            {
+                rows--;
+                count = 4 - count;
+                for(k = 1; k <= count; k++)
+                {
+                    printf("%d",k);
+                    rows++;
+                    if(k == count)
+                    {
+                        k--;
+                        for(; k > 0; k--)
+                        {
+                            printf("%d", k);
+                            rows++;
+                        }
+                        break;
+                    }
+                }
+                j = columns;
+             
+            }
+        }
+        printf("\n");
+    }
+}
 
 /*Question9.
 
@@ -86,6 +275,48 @@
     1 2 1
       1
 */
+#include<stdio.h>
+int main()
+{
+    int rows,columns,i = 4,j,count;
+    for(columns = 1; columns <= 4; columns++)
+    {
+        count = 0;
+        i = 5 - columns;
+        for(rows = 1; rows <= 7; rows++)
+        {
+            if(4 - i > 0)
+            {
+                printf(" ");
+                i++;
+                count++;
+            }
+            else
+            {
+                rows--;
+                i = i - count;
+                for(j = 1; j <= i; j++)
+                {
+                    printf("%d",j);
+                    rows++;
+                    if(j == i)
+                    {
+                        j--;
+                        for(; j > 0; j--)
+                        {
+                            printf("%d", j);
+                            rows++;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        printf("\n");
+
+    }
+}
+
 
 
 /*Question10.
@@ -96,6 +327,42 @@
 1           1
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,count;
+    for(columns = 1; columns <= 4; columns++)
+    {
+        count = 0;
+        for(rows = 1; rows <= 7; rows++)
+        {
+            if(rows <= 5 - columns)
+            {
+                printf("%d", rows);
+                count++;
+            }
+            else
+            {
+                if(count > 0 && columns != 1)
+                {
+                    rows--;
+                    count = ((4 - count) * 2) - 1;
+                    for(; count > 0; count--)
+                    {
+                        printf(" ");
+                        rows++;
+                    }
+                }
+                else
+                {
+                    i = 8 - rows;
+                    printf("%d", i);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
 
 /*Question11.
         A
@@ -105,7 +372,49 @@
 A B C D E D C B A
 
 */
-
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,count;
+    char ch;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        count = 0;
+        i = 1;
+        ch = 'A';
+        ch--;
+        for(rows = 1; rows <= 9; rows++)
+        {
+            if(i <= (5 - columns))
+            {
+                printf(" ");
+                count++;
+                i++;
+            }
+            else
+            {
+                if((5 - count) > 0)
+                {
+                    ch++;
+                    printf("%c", ch);
+                    count++;
+                }
+                else
+                {
+                    if(ch != 'A')
+                    {
+                        ch--;
+                        printf("%c",ch);
+                    }
+                    else
+                        i = 1;
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 /*Question12.
 
 A B C D C B A
@@ -114,6 +423,52 @@ A B C D C B A
       A
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,count;
+    char ch;
+    for(columns = 1; columns <= 4; columns++)
+    {
+        count = 0;
+        i = 1;
+        ch = 'A';
+        ch--;
+        for(rows = 1; rows <= 7; rows++)
+        {
+            if(columns - i > 0)
+            {
+                printf(" ");
+                count++;
+                i++;
+            }
+            else
+            {
+                if(4 - count > 0)
+                {
+                    ch++;
+                    printf("%c", ch);
+                    count++;
+                }
+                else
+                {
+                    if(ch != 'A')
+                    {
+                        ch--;
+                        printf("%c", ch);
+                    }
+                    else
+                    {
+                        i = 1;
+                        rows--;
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question13.
 
@@ -126,7 +481,48 @@ A B                   B A
 A                       A
 
 */
-
+#include<stdio.h>
+int main()
+{
+    int columns,rows,count;
+    char ch;
+    for(columns = 1; columns <= 7; columns++)
+    {
+        count = 0;
+        ch = 'A';
+        ch--;
+        for(rows = 1; rows <= 13; rows++)
+        {
+            if(rows <= 8 - columns)
+            {
+                ch++;
+                printf("%c", ch);
+                count++;
+            }
+            else
+            {
+                if(count > 0 && columns != 1)
+                {
+                    rows--;
+                    count = ((7 - count) * 2) - 1;
+                    for(; count > 0 ; count--)
+                    {
+                        printf(" ");
+                        rows++;
+                    }
+                    ch += 1;
+                }
+                else
+                {
+                    ch--;
+                    printf("%c", ch);
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 /*Question14.
 
 *
@@ -136,6 +532,23 @@ A                       A
 * * * * * 
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        for(rows = 1; rows <= 5; rows++)
+        {
+            if(rows == 1 || rows == columns || columns == 5)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question15.
 
@@ -146,6 +559,23 @@ A                       A
 * * * * *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        for(rows = 1; rows <= 5; rows++)
+        {
+            if(rows == 6 - columns || rows == 5 || columns == 5)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question16.
        *
@@ -155,6 +585,33 @@ A                       A
 * * * * * * * * *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,count;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        i = 5 - columns;
+        count = 0;
+        for(rows = 1; rows <= 9; rows++)
+        {
+            if(rows <= 5 - columns)
+            {
+                printf(" ");
+                count++;
+            }
+            else
+            {
+                if(6 - columns == rows || columns != 1 && 9 - count == rows || columns == 5)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question17.
 
@@ -165,6 +622,23 @@ A                       A
         *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows;
+    for(columns = 1; columns <= 5; columns++)
+    {
+        for(rows = 1; rows <= 9; rows++)
+        {
+            if(rows == columns || rows == 10 - columns || columns == 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 /*Question18.
 
@@ -179,6 +653,39 @@ A                       A
         *
 
 */
+#include<stdio.h>
+int main()
+{
+    int columns,rows,i,round;
+    for(columns = 1; columns <= 9; columns++)
+    {
+        round = columns;
+        if(columns > 5)
+            columns = 10 - columns;
+        i = 1;
+        for(rows = 1; rows <= 9; rows++)
+        {
+            if(i <= 5 - columns)
+            {
+                printf(" ");
+                i++;
+            }
+            else
+            {
+                rows--;
+                for(i = 1; i <= (columns * 2) - 1; i++)
+                {
+                    printf("*");
+                    rows++;
+                }
+                i = 1;
+            }
+        }
+        printf("\n");
+        columns = round;
+    }
+    return 0;
+}
 
 /*Question19.
 
@@ -192,5 +699,5 @@ A                       A
         * * * * * * * 
           * * * * * 
             * * * 
-              * * 
-                *
+              * 
+                
